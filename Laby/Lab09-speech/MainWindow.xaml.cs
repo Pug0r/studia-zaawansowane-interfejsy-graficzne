3d1b1b2f0a089c71a9ffb462c0d733e500587139
Aleksander Pugowski <aleksanderpugowski@gmail.com>
2026-05-24 18:41:37 +0200

    lab09 - load and read from file

--- Laby/Lab09-speech/MainWindow.xaml.cs
+++ Laby/Lab09-speech/MainWindow.xaml.cs
@@ -1,13 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.IO;
+using System.Speech.Synthesis;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace Lab09_speech
 {
@@ -16,9 +11,70 @@ namespace Lab09_speech
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly SpeechSynthesizer _synth = new();
+        private bool _isPlaying;
+
         public MainWindow()
         {
             InitializeComponent();
+            _synth.SpeakCompleted += (_, _) => Dispatcher.Invoke(() =>
+            {
+                _isPlaying = false;
+                UpdatePlayStop();
+            });
+            UpdatePlayStop();
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+            };
+
+            if (dialog.ShowDialog() != true)
+            {
+                return;
+            }
+
+            TextContent.Text = File.ReadAllText(dialog.FileName);
+            _synth.SpeakAsyncCancelAll();
+            _isPlaying = false;
+            UpdatePlayStop();
+        }
+
+        private void PlayStop_Click(object sender, RoutedEventArgs e)
+        {
+            var text = TextContent.Text;
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return;
+            }
+
+            if (_isPlaying)
+            {
+                _synth.SpeakAsyncCancelAll();
+                _isPlaying = false;
+            }
+            else
+            {
+                _synth.SpeakAsync(text);
+                _isPlaying = true;
+            }
+
+            UpdatePlayStop();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            _synth.Dispose();
+            base.OnClosed(e);
+        }
+
+        private void UpdatePlayStop()
+        {
+            PlayStopButton.IsEnabled = !string.IsNullOrWhiteSpace(TextContent.Text);
+            PlayStopButton.Content = _isPlaying ? "Stop" : "Play";
         }
     }
 }
