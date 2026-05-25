6e687878e6f8a818d85324a6cdb3123778de92a5
Aleksander Pugowski <aleksanderpugowski@gmail.com>
2026-05-25 19:40:10 +0200

    lab10 - add basic version of game board

---
+++ Laby/Lab10-gra/MainViewModel.cs
@@ -0,0 +1,69 @@
+using Lab06_gamelib;
+using Lab06_gamelib.Models;
+using Lab06_gamelib.Services;
+using System.Collections.ObjectModel;
+
+namespace Lab10_gra
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<BoardCellViewModel> BoardCells { get; } = new();
+        public string RoundLabel { get; }
+        public string PlayerLabel { get; }
+        public string CreditsLabel { get; }
+        public string GalacticTicketsLabel { get; }
+        public string PirateDefenseCardLabel { get; }
+        public string LogLabel { get; }
+
+        public MainViewModel()
+        {
+            var boardService = new BoardService();
+            var world = boardService.BuildWorld(new GameSettings());
+
+            for (int y = 0; y < world.Board.Size; y++)
+            {
+                for (int x = 0; x < world.Board.Size; x++)
+                {
+                    var field = world.Board.GetField(x, y);
+                    BoardCells.Add(new BoardCellViewModel(field, x, y));
+                }
+            }
+
+            RoundLabel = "Round: 1";
+            PlayerLabel = "Player: Player";
+            CreditsLabel = "Credits: 0";
+            GalacticTicketsLabel = "Galactic tickets: 0";
+            PirateDefenseCardLabel = "Pirate defense card: none";
+            LogLabel = "Game actions will appear here.";
+        }
+    }
+
+    public class BoardCellViewModel
+    {
+        public int X { get; }
+        public int Y { get; }
+        public string Name { get; }
+        public FieldKind Kind { get; }
+        public string Detail { get; }
+        public string Icon { get; }
+
+        public BoardCellViewModel(Field field, int x, int y)
+        {
+            X = x;
+            Y = y;
+            Name = field.Name;
+            Kind = field.Kind;
+            Detail = field.Kind == FieldKind.Planet && field.SystemId != null
+                ? $"System {field.SystemId}"
+                : field.Kind.ToString();
+            Icon = field.Kind switch
+            {
+                FieldKind.Planet => "🪐",
+                FieldKind.RailStop => "🚆",
+                FieldKind.PirateAttack => "🏴‍☠️",
+                FieldKind.Singularity => "✨",
+                _ => "·"
+            };
+        }
+    }
+}
