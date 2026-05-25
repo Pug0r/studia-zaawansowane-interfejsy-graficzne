78d95308f4744d0b344f1132f0a87be426209baf
Aleksander Pugowski <aleksanderpugowski@gmail.com>
2026-05-25 19:49:55 +0200

    lab10 - add control buttons

--- Laby/Lab10-gra/MainViewModel.cs
+++ Laby/Lab10-gra/MainViewModel.cs
@@ -2,6 +2,7 @@ using Lab06_gamelib;
 using Lab06_gamelib.Models;
 using Lab06_gamelib.Services;
 using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace Lab10_gra
 {
@@ -14,6 +15,19 @@ namespace Lab10_gra
         public string GalacticTicketsLabel { get; }
         public string PirateDefenseCardLabel { get; }
         public string LogLabel { get; }
+        public Visibility TakeTurnVisibility { get; } = Visibility.Visible;
+        public Visibility SkipTurnVisibility { get; } = Visibility.Visible;
+        public Visibility PayRansomVisibility { get; } = Visibility.Collapsed;
+        public Visibility UsePirateDefenseVisibility { get; } = Visibility.Collapsed;
+        public Visibility DeclinePirateDefenseVisibility { get; } = Visibility.Collapsed;
+        public Visibility RailStopVisibility { get; } = Visibility.Collapsed;
+        public Visibility BuildPortVisibility { get; } = Visibility.Collapsed;
+        public Visibility UpgradeSettlementVisibility { get; } = Visibility.Collapsed;
+        public Visibility UpgradeMineVisibility { get; } = Visibility.Collapsed;
+        public Visibility UpgradeFarmVisibility { get; } = Visibility.Collapsed;
+        public Visibility BuildShipyardVisibility { get; } = Visibility.Collapsed;
+        public Visibility UpgradeAsteroidMineVisibility { get; } = Visibility.Collapsed;
+        public Visibility WaitVisibility { get; } = Visibility.Collapsed;
 
         public MainViewModel()
         {
