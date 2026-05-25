9dbcafe0bf90bcbdddec4a9257876b1186b15e6a
Aleksander Pugowski <aleksanderpugowski@gmail.com>
2026-05-25 19:23:55 +0200

    lab10 - init

---
+++ Laby/Lab10-gra/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Lab10_gra
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
